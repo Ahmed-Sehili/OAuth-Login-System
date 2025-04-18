8f1459dde40d5d8ed8bec298ef4f2ced2e7257bb
Ahmed <ahmedsehili@outlook.com>
2025-04-18 22:46:02 +0100

    Update home page with animated background & better UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,30 @@ import { SignInButton, SignUpButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-[80vh] px-4 text-center">
-      <section className="max-w-2xl">
-        <h1 className="text-5xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text">
-          Welcome to OAuth Login System
-        </h1>
-        <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">
-          Securely authenticate users with OAuth, Clerk, and Next.js. Fast,
-          modern, and easy to use. Get started by signing up or logging in!
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <SignUpButton>
-            <button className="px-6 py-3 rounded-lg bg-blue-600 text-white font-semibold shadow hover:bg-blue-700 transition order-1 sm:order-none">
-              Create an Account
-            </button>
-          </SignUpButton>
-          <SignInButton>
-            <button className="px-6 py-3 rounded-lg border border-blue-600 text-blue-600 font-semibold hover:bg-blue-50 dark:hover:bg-blue-900 transition order-2 sm:order-none">
-              Login
-            </button>
-          </SignInButton>
-        </div>
-      </section>
+      <div className="animated-gradient-bg" />
+      <div className="bg-gray-100 dark:bg-zinc-900/90 p-8 sm:p-12 rounded-3xl shadow-2xl backdrop-blur-md w-full max-w-4xl mx-auto">
+        <section className="max-w-3xl mx-auto sm:min-h-[400px] flex flex-col justify-center gap-8">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text">
+            Welcome to my Login System
+          </h1>
+          <p className="text-base sm:text-lg md:text-xl text-gray-400  mb-6">
+            Securely authenticate users with OAuth, Clerk, and Next.js. Fast,
+            modern, and easy to use. Get started by signing up or logging in!
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <SignUpButton>
+              <button className="cursor-pointer px-6 py-3 rounded-lg bg-blue-600 text-white font-semibold shadow hover:bg-blue-700 transition order-1 sm:order-none">
+                Create an Account
+              </button>
+            </SignUpButton>
+            <SignInButton>
+              <button className="cursor-pointer px-6 py-3 rounded-lg text-blue-600 font-semibold hover:bg-blue-50 dark:hover:bg-blue-900 transition order-2 sm:order-none">
+                Already have an account?
+              </button>
+            </SignInButton>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
